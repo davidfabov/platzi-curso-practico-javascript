825246e759823b0b4a782c32c3ed2b650fe60eed
davidfabov <davidfbv2010@gmail.com>
2021-08-23 10:33:45 -0500

    Initial commit

--- descuentos.js
+++ descuentos.js
@@ -4,3 +4,16 @@ function calcularPrecioConDescuento(precio, descuento) {
 
     return precioConDescuento;
 }
+
+function outputPriceDiscount() {
+    const inputPrice = document.getElementById("InputPrice");
+    const priceValue = inputPrice.value;
+
+    const inputDiscount = document.getElementById("InputDiscount");
+    const discountValue = inputDiscount.value;
+
+    const precioConDescuento = calcularPrecioConDescuento(priceValue, discountValue);
+
+    const result = document.getElementById("ResultParagraph");
+    result.innerText = "El precio con descuento es: $" + precioConDescuento;
+}
