424ecb4e0d4a268c44a7d014a2f7a227dab39f51
davidfabov <davidfbv2010@gmail.com>
2021-08-25 10:20:52 -0500

    Initial commit

---
+++ salary.js
@@ -0,0 +1,31 @@
+const colombia = [];
+
+colombia.push({
+    name: 'Daniel',
+    salary: 1000,
+});
+
+colombia.push({
+    name: 'Sarah',
+    salary: 1500,
+});
+
+colombia.push({
+    name: 'Carlos',
+    salary: 850,
+});
+
+colombia.push({
+    name: 'Natalia',
+    salary: 900,
+});
+
+colombia.push({
+    name: 'Valentina',
+    salary: 1000,
+});
+
+colombia.push({
+    name: 'Luis',
+    salary: 1200,
+});
